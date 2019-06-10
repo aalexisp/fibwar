be440b8cb44d9d943bd26b714e811e659cceba8d
dabitsr <43137289+dabitsr@users.noreply.github.com>
2019-06-10 16:20:47 +0200

    Add files via upload

---
+++ fibwar.py
@@ -0,0 +1,105 @@
+#import tweepy
+from time import sleep
+import random
+
+#auth = tweepy.OAuthHandler("", "")
+#auth.set_access_token("", "")
+
+#api = tweepy.API(auth)
+
+def guerra(): #(api)
+    tiempoEmpezar = 180
+    tiempo = 30
+    jugadores = {"David":random.randint(0,9),
+    "Dani":random.randint(0,4),
+    "Sergi":random.randint(0,4),
+    "Alexis":random.randint(0,4),
+    "Ferri":random.randint(0,4)}
+
+    listaJugadores = ["David", "Dani", "Alexis", "Sergi", "Ferri"]
+    listaMuertos = []
+    while len(jugadores) > 1:
+        if len(jugadores) < 5:
+            resucitar = random.randint(0,1000)
+            if resucitar < 250:
+                i = random.randint(0,len(listaMuertos)-1)
+                resucitado = listaMuertos[i]
+                jugadores.update({resucitado:random.randint(0,4)})
+                listaJugadores.append(resucitado)
+                listaMuertos.pop(i)
+                print(resucitado + " ha resucitado gracias a la fuerza de los bocatas del bar de la FIB!")
+                #foto = resucitado + ".jpeg"
+                #status = resucitado + " ha resucitado gracias a la fuerza de los bocatas del bar de la FIB!"
+                #api.update_with_media(foto, status)
+                sleep(tiempo)
+
+        i = random.randint(0,len(listaJugadores)-1)
+        j = random.randint(0,len(listaJugadores)-1)
+        while i == j:
+            j = random.randint(0,len(listaJugadores)-1)
+
+        ataca1 = listaJugadores[i]
+        ataca2 = listaJugadores[j]
+
+        ataque1 = random.randint(0, 100)
+        ataque2 = random.randint(0, 100)
+        if jugadores[ataca1]*ataque1 > jugadores[ataca2]*ataque2:
+            jugadores[ataca1] += random.randint(0,3)
+            #foto = ataca1 + ".jpeg"
+            #status = ataca1 + " ha eliminado a " + ataca2
+            #api.update_with_media(foto, status)
+            print(ataca1 + " ha eliminado a " + ataca2)
+            listaJugadores.pop(j)
+            listaMuertos.append(ataca2)
+            jugadores.pop(ataca2)
+            ganador = ataca1
+
+        elif jugadores[ataca1]*ataque1 < jugadores[ataca2]*ataque2:
+            jugadores[ataca2] += random.randint(0,3)
+            #foto = ataca2 + ".jpeg"
+            #status = ataca2 + " ha eliminado a " + ataca1
+            #api.update_with_media(foto, status)
+            print(ataca2 + " ha eliminado a " + ataca1)
+            listaJugadores.pop(i)
+            listaMuertos.append(ataca1)
+            jugadores.pop(ataca1)
+            ganador = ataca2
+
+        else:
+            aux = [ataca1, ataca2]
+            aux2 = random.randint(0,1)
+
+            if aux2 == 0:
+                #foto = ataca2 + ".jpeg"
+                #status = ataca2 + " ha eliminado a " + ataca1
+                #api.update_with_media(foto, status)
+                print(ataca2 + " ha eliminado a " + ataca1)
+                jugadores[ataca2] += random.randint(0,3)
+                ganador = ataca2
+                listaJugadores.pop(aux2)
+                listaMuertos.append(ataca1)
+                jugadores.pop(ataca1)
+            else:
+                #foto = ataca1 + ".jpeg"
+                #status = ataca1 + " ha eliminado a " + ataca2
+                #api.update_with_media(foto, status)
+                print(ataca1 + " ha eliminado a " + ataca2)
+                jugadores[ataca1] += random.randint(0,3)
+                ganador = ataca1
+                listaJugadores.pop(aux2)
+                listaMuertos(ataca2)
+                jugadores.pop(ataca2)
+
+        if len(jugadores) > 1:
+            sleep(tiempo)
+    return ganador
+
+#api.update_status("La guerra empieza en 3 minutos!!")
+print("La guerra empieza en 3 minutos!!")
+sleep(tiempoEmpezar)
+ganador = guerra() #(api)
+
+#foto = ganador + ".jpeg"
+#status = ganador + " ha ganado la guerra!!"
+#api.update_with_media(foto, status)
+print(ganador + " ha ganado la guerra!")
